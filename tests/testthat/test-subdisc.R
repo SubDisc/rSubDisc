88710e8974b9200b4c3f1095a4d544c60da894f8
Jeremie Gobeil <j.gobeil@liacs.leidenuniv.nl>
2021-12-20 16:24:09 +0100

    Changed args to str in function calls

--- tests/testthat/test-subdisc.R
+++ tests/testthat/test-subdisc.R
@@ -1,5 +1,37 @@
 rSubDisc.file <- function(...) system.file(..., package = "rSubDisc")
 
+
+# General function -------------------------------------------------------------
+
+test_that("Function subgroupdiscovery - single nominal", {
+  testdatafile = rSubDisc.file("extdata", "adult.txt")
+
+  testAdult <- subgroupdiscovery(
+    src = testdatafile,
+    targetColumn = 14,
+    targetValue = "gr50K",
+    targetType = "SINGLE_NOMINAL",
+    qualityMeasure = "CORTANA_QUALITY",
+    qualityMeasureMinimum = 0.1,
+    searchDepth = 1,
+    minimumCoverage = 2,
+    maximumCoverageFraction = 1.0,
+    maximumSubgroups = 1000,
+    maximumTime = 1000,
+    searchStrategy = "BEAM",
+    nominalSets = FALSE,
+    numericOperatorSetting = "NORMAL",
+    numericStrategy = "NUMERIC_BEST",
+    searchStrategyWidth = 10,
+    nrBins = 8,
+    nrThreads = 1
+  )
+
+  expect_equal(testdata_single_nominal, testAdult)
+})
+
+# Simple functions -------------------------------------------------------------
+
 test_that("Test single nominal", {
   testdatafile = rSubDisc.file("extdata", "adult.txt")
   testAdult <- .subdisc.single_nominal.cortana_quality(
